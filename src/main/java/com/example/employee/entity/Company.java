4b65864c0996d1158eeb873a19274f2e0e268a83
刘云志 <liuyunzhi@chinamcloud.com>
2018-05-05 00:48:59 +0800

    完成练习

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,48 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Company {
 
+    @Id
+    private Integer id;
+    private String companyName;
+    private Integer employeeNumber;
+
+    public Company(){
+        super();
+    }
+
+    public Company(Integer id, String companyName, Integer employeeNumber){
+
+        this.id = id;
+        this.companyName = companyName;
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
 }
